51704a9d647b22f06c1ca746fad1c430a34b5b0b
Bastianleaf <bastian.martinez@ing.uchile.cl>
2017-06-26 21:52:04 -0400

    Imagen de editar producto arreglado.

--- tarea3/main/views.py
+++ tarea3/main/views.py
@@ -357,7 +357,7 @@ class editarproductos(View):
                 with default_storage.open('../media/productos/' + filename, 'wb+') as destination:
                     for chunk in imagen.chunks():
                         destination.write(chunk)
-            Comida.objects.filter(nombre=nombreOriginal).update(nombre=nombre,descripcion=descripcion,precio=precio,stock=stock,imagen='avatars/' + filename,categorias=categorias)
+            Comida.objects.filter(nombre=nombreOriginal).update(nombre=nombre,descripcion=descripcion,precio=precio,stock=stock,imagen='productos/' + filename,categorias=categorias)
         except:
             return self.get(request)
 
